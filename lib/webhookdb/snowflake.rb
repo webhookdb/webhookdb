1119441385d8f172a62a34a6ae81383b8b717246
Rob Galanakis <rob.galanakis@gmail.com>
2022-05-28 15:09:56 -0700

    Snowsql output can be parsed

--- lib/webhookdb/snowflake.rb
+++ lib/webhookdb/snowflake.rb
@@ -21,7 +21,7 @@ class Webhookdb::Snowflake
   # - Any query param matching account/user/db/schema/role is used.
   # - URI hostname is used as accountname, basic auth user as username, and uri path as dbname.
   # - Password is pulled from query param 'password' or uri basic auth password.
-  def self.parse_url_to_cli_args(url)
+  def self.parse_url_to_cli_args(url, format: "json")
     uri = URI(url)
     params = Rack::Utils.parse_query(uri.query)
     password = params["password"] || uri.password
@@ -29,7 +29,8 @@ class Webhookdb::Snowflake
     cli = [
       self.snowsql,
       "-o", "friendly=false",
-      "-o", "quiet=true",
+      "-o", "output_format=#{format}",
+      "-o", "timing=false",
       "--accountname", params["accountname"] || params["account"] || uri.hostname || "",
       "--username", params["username"] || params["user"] || uri.user || "",
       "--dbname", params["dbname"] || params["db"] || uri.path&.delete_prefix("/") || "",
@@ -46,19 +47,43 @@ class Webhookdb::Snowflake
     return cli, {"SNOWSQL_PWD" => password}
   end
 
-  def self.run_cli(url, query)
-    args, env = self.parse_url_to_cli_args(url)
-    file = Tempfile.new("whdbsnowql")
-    file.write(query)
-    file.close
-    args.concat(["-f", file.path])
+  def self.run_cli(url, query, parse: false, format: "json")
+    args, env = self.parse_url_to_cli_args(url, format:)
+    args.concat(["-q", query])
     stdout, stderr, status = Open3.capture3(env, *args)
-    file.unlink
 
-    return if stderr.blank? && status.success?
+    if stderr.blank? && status.success?
+      result = if parse.respond_to?(:call)
+                 parse.call(stdout)
+      elsif parse && format == "json"
+        self._parse_json(stdout)
+      else
+        stdout
+      end
+      return result
+    end
 
     self.logger.error("snowflake_error", stdout:, stderr:, status:, cli_args: args)
-    msg = "status: #{status}, stderr: #{stderr}, stdout: #{stdout}"
+    msg = "status: #{status}, stderr: #{stderr}, stdout: #{stdout}, query: #{query}"
     raise Webhookdb::InvalidPostcondition, "snowflake failed: #{msg}"
+  end
+
+  # We can't parse newline delimited json easily, so split it ourselves and parse each document.
+  def self._parse_json(stdout)
+    docs = stdout.split("]\n[")
+    result = docs.each_with_index.map do |j, i|
+      if docs.size > 1
+        if i.zero?
+          j += "]"
+        else
+          j = "[" + j
+        end
+      end
+      Yajl::Parser.parse(j.strip)
+    end
+    return result
+  rescue StandardError => e
+    msg = "error: #{e}, stdout: #{stdout}"
+    raise Webhookdb::InvalidPostcondition, "Error parsing snowsql output: #{msg}"
   end
 end
--- spec/webhookdb/snowflake_spec.rb
+++ spec/webhookdb/snowflake_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Webhookdb::Snowflake, :db do
         password: "p",
       }.to_query)
       expect(args[0]).to eq(
-        ["snowbin", "-o", "friendly=false", "-o", "quiet=true", "--accountname", "a",
+        ["snowbin", "-o", "friendly=false", "-o", "output_format=json", "-o", "timing=false", "--accountname", "a",
          "--username", "u", "--dbname", "d", "--schemaname", "s", "--rolename", "r", "--warehouse", "w",],
       )
       expect(args[1]).to eq({"SNOWSQL_PWD" => "p"})
@@ -40,7 +40,7 @@ RSpec.describe Webhookdb::Snowflake, :db do
         password: "p",
       }.to_query)
       expect(args[0]).to eq(
-        ["snowbin", "-o", "friendly=false", "-o", "quiet=true", "--accountname", "a",
+        ["snowbin", "-o", "friendly=false", "-o", "output_format=json", "-o", "timing=false", "--accountname", "a",
          "--username", "u", "--dbname", "d", "--schemaname", "s", "--rolename", "r", "--warehouse", "w",],
       )
       expect(args[1]).to eq({"SNOWSQL_PWD" => "p"})
@@ -49,7 +49,7 @@ RSpec.describe Webhookdb::Snowflake, :db do
     it "falls back to the uri" do
       args = described_class.parse_url_to_cli_args("snowflake://u:p@a/d")
       expect(args[0]).to eq(
-        ["snowbin", "-o", "friendly=false", "-o", "quiet=true", "--accountname", "a",
+        ["snowbin", "-o", "friendly=false", "-o", "output_format=json", "-o", "timing=false", "--accountname", "a",
          "--username", "u", "--dbname", "d",],
       )
       expect(args[1]).to eq({"SNOWSQL_PWD" => "p"})
@@ -72,7 +72,37 @@ RSpec.describe Webhookdb::Snowflake, :db do
     break unless described_class.run_tests
 
     it "can connect to the test url" do
-      described_class.run_cli(described_class.test_url, "SELECT COUNT(1) FROM QUERYCHECKER")
+      res = described_class.run_cli(described_class.test_url, "SELECT COUNT(1) FROM QUERYCHECKER")
+      expect(res).to be_present
+    end
+
+    it "can parse json output" do
+      res = described_class.run_cli(described_class.test_url, "SELECT 1", parse: true)
+      expect(res).to eq([[{"1" => "1"}]])
+    end
+
+    it "can parse output that contains json" do
+      res = described_class.run_cli(
+        described_class.test_url,
+        'SELECT parse_json(\'{"x": {"y": 1}}\') as X',
+        parse: true,
+      )
+      expect(res).to eq([[{"X" => "{\n  \"x\": {\n    \"y\": 1\n  }\n}"}]])
+    end
+
+    it "can parse output that contains multiple json lines" do
+      res = described_class.run_cli(
+        described_class.test_url,
+        'SELECT parse_json(\'{"x": {"y": 1}}\') AS x;SELECT parse_json(\'{"x": {"y": 1}}\') AS x',
+        parse: true,
+      )
+      expect(res).to eq([[{"X" => "{\n  \"x\": {\n    \"y\": 1\n  }\n}"}],
+                         [{"X" => "{\n  \"x\": {\n    \"y\": 1\n  }\n}"}],])
+    end
+
+    it "can use a custom parser/format" do
+      res = described_class.run_cli(described_class.test_url, "SELECT 1", parse: ->(s) { CSV.parse(s) }, format: "csv")
+      expect(res).to eq([["1"], ["1"]])
     end
 
     it "raises errors from Snowflake" do
