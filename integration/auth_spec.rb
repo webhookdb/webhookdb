76890536a336f1fc9998f99e4d8231b0a8669db0
Rob Galanakis <rob.galanakis@gmail.com>
2022-06-13 01:32:41 -0700

    Re-enable integration tests

--- integration/auth_spec.rb
+++ integration/auth_spec.rb
@@ -3,72 +3,41 @@
 RSpec.describe "auth", :integration do
   let(:password) { Webhookdb::Fixtures::Customers::PASSWORD }
 
-  it "allows me to sign up" do
+  it "allows me to sign up with a token, and log out" do
+    customer = Webhookdb::Fixtures.customer.create
+    code = Webhookdb::Fixtures.reset_code(customer:).create
+
+    login_resp = post(
+      "/v1/auth",
+      body: {email: customer.email, token: code.token},
+    )
+    expect(login_resp).to party_status(200)
+
+    customer_resp = get("/v1/me")
+    expect(customer_resp).to party_status(200)
+
+    logout_resp = post("/v1/auth/logout")
+    expect(logout_resp).to party_status(200)
+  end
+
+  it "allows me to login via OTP, and logout" do
     customer = Webhookdb::Fixtures.customer.instance
 
-    login_resp = post(
-      "/v1/register",
-      body: {
-        email: customer.email,
-        password:,
-        phone: customer.phone,
-        timezone: "America/Los_Angeles",
-      },
-    )
+    login_resp = post("/v1/auth", body: {email: customer.email})
+    expect(login_resp).to party_status(202)
+
+    customer = Webhookdb::Customer[email: customer.email]
+    login_resp = post("/v1/auth/login_otp/#{customer.opaque_id}", body: {value: customer.reset_codes.last.token})
     expect(login_resp).to party_status(200)
 
     customer_resp = get("/v1/me")
     expect(customer_resp).to party_status(200)
+
+    logout_resp = post("/v1/auth/logout")
+    expect(logout_resp).to party_status(200)
   end
 
-  it "allows me to log in and out" do
-    customer = Webhookdb::Fixtures.customer.password(password).create
-
-    login_resp = post("/v1/auth", body: {email: customer.email, password:})
-    expect(login_resp).to party_status(200)
-    expect(login_resp).to party_response(match(hash_including(name: customer.name)))
-
-    customer_resp = get("/v1/me")
-    expect(customer_resp).to party_status(200)
-
-    logout_resp = delete("/v1/auth")
-    expect(logout_resp).to party_status(204)
-  end
-
-  it "signs me in if I sign up but already have an account with that email/password" do
-    customer = Webhookdb::Fixtures.customer.password(password).create
-
-    login_resp = post(
-      "/v1/register",
-      body: {
-        email: customer.email,
-        password:,
-        name: customer.name,
-        phone: customer.phone,
-        timezone: "America/Los_Angeles",
-      },
-    )
-    expect(login_resp).to party_status(200)
-    expect(login_resp).to party_response(match(hash_including(id: customer.id)))
-  end
-
-  it "can forget and reset a password" do
-    customer = Webhookdb::Fixtures.customer.create
-
-    forgot_resp = post("/v1/me/forgot_password", body: {email: customer.email})
-    expect(forgot_resp).to party_status(202)
-
-    expect(customer.reset_codes).to have_attributes(length: 1)
-    token = customer.reset_codes.first
-
-    reset_resp = post("/v1/me/reset_password", body: {token: token.token, password: "test1234reset"})
-    expect(reset_resp).to party_status(200)
-
-    get_customer_resp = get("/v1/me")
-    expect(get_customer_resp).to party_status(200)
-  end
-
-  xit "can access admin endpoints only if the customer authed as an admin and retains the role" do
+  it "can access admin endpoints only if the customer authed as an admin and retains the role" do
     customer = Webhookdb::Fixtures.customer.admin.instance
     auth_customer(customer)
 
@@ -79,58 +48,6 @@ RSpec.describe "auth", :integration do
     customer.remove_role(Webhookdb::Role.admin_role)
 
     resp = get("/admin/v1/auth")
-    expect(resp).to party_status(403)
-  end
-
-  xit "can sudo, re-sudo, and unsudo" do
-    admin = Webhookdb::Fixtures.customer.admin.instance
-    auth_customer(admin)
-
-    target = Webhookdb::Fixtures.customer.create
-
-    sudo_resp = post("/admin/v1/auth/sudo/#{target.id}")
-    expect(sudo_resp).to party_status(200)
-    expect(sudo_resp).to party_response(match(hash_including(name: target.name, sudoed: true)))
-
-    get_customer_resp = get("/v1/me")
-    expect(get_customer_resp).to party_status(200)
-    expect(get_customer_resp).to party_response(match(hash_including(name: target.name, sudoed: true)))
-
-    get_admin_resp = get("/admin/v1/auth")
-    expect(get_admin_resp).to party_status(200)
-    expect(get_admin_resp).to party_response(match(hash_including(name: admin.name, sudoed: true)))
-
-    other_target = Webhookdb::Fixtures.customer.create
-    resudo_resp = post("/admin/v1/auth/sudo/#{other_target.id}")
-    expect(resudo_resp).to party_status(200)
-    expect(resudo_resp).to party_response(match(hash_including(name: other_target.name, sudoed: true)))
-
-    unsudo_resp = delete("/admin/v1/auth/sudo")
-    expect(unsudo_resp).to party_status(200)
-    expect(unsudo_resp).to party_response(match(hash_including(name: admin.name, sudoed: false)))
-
-    get_customer2_resp = get("/v1/me")
-    expect(get_customer2_resp).to party_status(200)
-    expect(get_customer2_resp).to party_response(match(hash_including(name: admin.name, sudoed: false)))
-  end
-
-  xit "can sudo a deleted customer" do
-    admin = Webhookdb::Fixtures.customer.admin.instance
-    auth_customer(admin)
-
-    target = Webhookdb::Fixtures.customer.create
-    target.soft_delete
-
-    sudo_resp = post("/admin/v1/auth/sudo/#{target.id}")
-    expect(sudo_resp).to party_status(200)
-    expect(sudo_resp).to party_response(match(hash_including(name: target.name, sudoed: true)))
-
-    get_customer_resp = get("/v1/me")
-    expect(get_customer_resp).to party_status(200)
-    expect(get_customer_resp).to party_response(match(hash_including(name: target.name, sudoed: true)))
-
-    get_admin_resp = get("/admin/v1/auth")
-    expect(get_admin_resp).to party_status(200)
-    expect(get_admin_resp).to party_response(match(hash_including(name: admin.name, sudoed: true)))
+    expect(resp).to party_status(401)
   end
 end
