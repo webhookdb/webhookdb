86e14fc0bf6ad2eff77645d002024abea40c723a
Rob Galanakis <rob.galanakis@gmail.com>
2023-02-21 10:25:20 -0800

    Rewrite DurableJob to move jobs to dead set
    Fixes https://github.com/lithictech/webhookdb-api/issues/598
    
    Reimplements the re-enqueue mechanism to instead move the job
    to the deadset.
    
    See docs for some more explanation.

--- lib/webhookdb/async/job_logger.rb
+++ lib/webhookdb/async/job_logger.rb
@@ -6,4 +6,29 @@ class Webhookdb::Async::JobLogger < Appydays::Loggable::SidekiqJobLogger
   protected def slow_job_seconds
     return Webhookdb::Async.slow_job_seconds
   end
+
+  def self.durable_job_failure_notifier(job)
+    jcls = job["class"]
+    jargs = job["args"]
+    jid = job["jid"]
+    err = [job["error_class"], job["error_message"]].compact.join(": ")
+    Webhookdb::DeveloperAlert.new(
+      subsystem: "Job Died",
+      emoji: ":zombie: ",
+      fallback: "Job #{jcls}[#{jid}][#{jargs}] moved to DeadSet: #{err}",
+      fields: [
+        {title: "Job ID", value: jid, short: true},
+        {title: "Job Class", value: jcls, short: true},
+        {title: "Args", value: jargs.to_json},
+        {title: "Retry", value: job["retry"], short: true},
+        {title: "Queue", value: job["queue"], short: true},
+        {title: "Dead", value: job["dead"], short: true},
+        {title: "Created", value: job["created_at"], short: true},
+        {title: "Enqueued", value: job["enqueued_at"], short: true},
+        {title: "Error", value: err, short: true},
+        {title: "Failed", value: job["failed_at"], short: true},
+        {title: "retries", value: job["retry_count"], short: true},
+      ],
+    ).emit
+  end
 end
