1f37ae1cbbd6fde462e839d17a7f65b3686290eb
Rob Galanakis <rob.galanakis@gmail.com>
2023-02-22 10:16:33 -0800

    Fix sponsy cron expression

--- lib/webhookdb/sponsy.rb
+++ lib/webhookdb/sponsy.rb
@@ -6,7 +6,7 @@ module Webhookdb::Sponsy
   include Appydays::Configurable
 
   configurable(:sponsy) do
-    setting :cron_expression, "*/30 */4 * * * *" # “At every 30th minute past every 4th hour.”
+    setting :cron_expression, "*/30 */4 * * *" # “At every 30th minute past every 4th hour.”
     setting :page_size, 100
   end
 end
