117b68239b13983389623e5d3f27385a93a5522f
Rob Galanakis <rob.galanakis@gmail.com>
2023-03-10 09:59:47 -0800

    Lower various splays, fix nextpax splay (was > cron duration)

--- lib/webhookdb/jobs/convertkit_broadcast_backfill.rb
+++ lib/webhookdb/jobs/convertkit_broadcast_backfill.rb
@@ -7,7 +7,7 @@ class Webhookdb::Jobs::ConvertkitBroadcastBackfill
   extend Webhookdb::Async::ScheduledJob
 
   cron "0 10 * * * *"
-  splay 10.minutes
+  splay 5.minutes
 
   def _perform
     Webhookdb::ServiceIntegration.dataset.where_each(service_name: "convertkit_broadcast_v1") do |sint|
