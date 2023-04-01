1689526fb353c9c88aaee56e7efc9b6ace5c6bad
Rob Galanakis <rob.galanakis@gmail.com>
2023-03-31 21:50:59 -0700

    AWS module cleanup
    - Remove s3, we don't use it (and won't).
      If we need uploads, we'll do it via R2 or something
      (and we have `Document` for now anyway).
    - Remove `Uploads` endpoints, since we don't use s3 anymore
      so don't need presigning.
    - Use structured logging in the AWS SDK.
      This was pretty tricky to get working.
    - Add STS to AWS SDK (will need this in the future,
      and we need it now for testing).

--- lib/webhookdb/aws.rb
+++ lib/webhookdb/aws.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "aws-sdk-core"
+require "aws-sdk-sts"
 require "appydays/configurable"
 
 require "webhookdb" unless defined?(Webhookdb)
@@ -10,11 +11,59 @@ module Webhookdb::AWS
   include Appydays::Configurable
   include Appydays::Loggable
 
-  require "webhookdb/aws/s3"
-  singleton_attr_reader :s3
+  class ShimLogger
+    def initialize(logger, operation_prefix: "aws_", level: :debug, error_level: :warn)
+      @logger = logger
+      @param_formatter = ::Aws::Log::ParamFormatter.new({})
+      @param_filter = ::Aws::Log::ParamFilter.new({})
+      @operation_prefix = operation_prefix
+      @level = level
+      @error_level = error_level
+    end
 
-  # Some stuff doesn't work right with explicit config, so force-set the ENV
+    # Logger method. Receives the unformatted response form the shim formatter,
+    # and logs a structured log.
+    # @param [Seahorse::Client::Response] response
+    def logshim(response)
+      level = @level
+      msg = @operation_prefix + response.context.operation_name.to_s
+      params = response.context.params
+      type = response.context.operation.input.shape.struct_class
+      ctx = {
+        aws_client: (response.context.client.class.name || "").delete_prefix("Aws::").delete_suffix("::Client"),
+        http_response_code: response.context.http_response.status_code,
+        elapsed: response.context[:logging_completed_at] - response.context[:logging_started_at],
+        request_params: @param_formatter.summarize(@param_filter.filter(params, type)),
+      }
+      ctx[:retries] = response.context.retries if response.context.retries.positive?
+      if response.error
+        level = @error_level
+        ctx[:error_class] = response.error.class.name
+        ctx[:error_message] = response.error.message
+      end
+      @logger.send(level, msg, ctx)
+    end
+  end
+
+  class ShimFormatter
+    # Normally this must return a string, but we need to have it return a raw response
+    # for use in the shim logger.
+    # @param [Seahorse::Client::Response] response
+    # @return [String]
+    def format(response)
+      # noinspection RubyMismatchedReturnType
+      return response
+    end
+  end
+
+  singleton_attr_reader :sts_client
+
   configurable(:aws) do
+    # WebhookDB's AWS Account ID.
+    # Used for things like cross-account role assumption.
+    setting :external_account_id, "054088425385"
+
+    # Some stuff doesn't work right with explicit config, so force-set the ENV
     setting :access_key_id,
             "default-access",
             key: "AWS_ACCESS_KEY_ID",
@@ -28,33 +77,21 @@ module Webhookdb::AWS
             key: "AWS_REGION",
             side_effect: ->(v) { ENV["AWS_REGION"] = v }
 
-    setting :default_s3_presign_expiration_secs, 300
-    setting :default_s3_presign_acl, "private"
-
     after_configured do
       ::Aws.config.update(
-        logger: self.logger,
+        logger: ShimLogger.new(self.logger),
+        log_formatter: ShimFormatter.new,
+        log_level: :logshim,
       )
 
-      @s3 = S3.new
+      @sts_client = Aws::STS::Client.new
     end
   end
 
-  S3_BUCKET_OVERRIDES = {
-    "webhookdb-public-1": {
-      presign_acl: "public-read",
-    },
-  }.freeze
-
-  # Return a hash of bucket-specific configuration where applicable, else global values.
-  def self.bucket_configuration_for(bucket_name)
-    config = S3_BUCKET_OVERRIDES[bucket_name.to_sym] || {}
-
-    return [
-      :presign_expiration_secs,
-      :presign_acl,
-    ].each_with_object({}) do |key, hash|
-      hash[key] = config[key] || self.send("default_s3_#{key}")
+  REGIONS_TO_LOCATIONS = Aws.partitions.each_with_object({}) do |partition, h|
+    partition.regions.each do |region|
+      h[region.name] = region.description
     end
-  end
+  end.freeze
+  LOCATIONS_TO_REGIONS = REGIONS_TO_LOCATIONS.to_a.map(&:reverse!).to_h.freeze
 end
--- spec/webhookdb/aws_spec.rb
+++ spec/webhookdb/aws_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "../spec_helper"
-
 require "webhookdb/aws"
 
 RSpec.describe Webhookdb::AWS do
@@ -20,222 +18,64 @@ RSpec.describe Webhookdb::AWS do
     end
   end
 
-  describe "#bucket_configuration_for" do
-    it "returns defaults when bucket-specific configuration does not exist" do
-      expect(described_class.bucket_configuration_for("potato-bucket")).to include(
-        presign_expiration_secs: 300,
+  it "maps regions and locations" do
+    expect(described_class::REGIONS_TO_LOCATIONS["us-west-2"]).to eq("US West (Oregon)")
+    expect(described_class::LOCATIONS_TO_REGIONS["US West (Oregon)"]).to eq("us-west-2")
+  end
+
+  describe "logging" do
+    it "logs in a structured format" do
+      stub_request(:post, "https://sts.us-east-2.amazonaws.com/").
+        to_return(status: 200, body: "", headers: {})
+
+      logs = capture_logs_from(described_class.logger, level: :debug, formatter: :json) do
+        described_class.sts_client.assume_role(
+          role_arn: "arn:aws:iam::05408855555:role/Testing",
+          role_session_name: "testing",
+        )
+      end
+      expect(logs).to contain_exactly(
+        include_json(
+          level: "debug",
+          name: "Webhookdb::AWS",
+          message: "aws_assume_role",
+          context: {
+            aws_client: "STS",
+            http_response_code: 200,
+            elapsed: be_positive,
+            request_params: "role_arn:\"arn:aws:iam::05408855555:role/Testing\",role_session_name:\"testing\"",
+          },
+        ),
       )
     end
 
-    it "returns bucket-specific configuration where applicable" do
-      expect(described_class.bucket_configuration_for("blah")).to include(presign_acl: "private")
-      expect(described_class.bucket_configuration_for("webhookdb-public-1")).to include(presign_acl: "public-read")
-    end
-  end
+    it "logs errors" do
+      stub_request(:post, "https://sts.us-east-2.amazonaws.com/").
+        to_return(status: 400, body: "", headers: {})
 
-  describe "S3" do
-    before(:each) do
-      allow(described_class.s3).to receive(:client).and_return(Aws::S3::Client.new(stub_responses: true))
-    end
-
-    describe "#metadata" do
-      it "returns metadata about an existing object" do
-        metadata = described_class.s3.metadata("some-bucket", "some-key")
-        expect(metadata[:content_length]).to eq(0) # default key and value in a stubbed response
-      end
-    end
-
-    describe "#exists?" do
-      it "is true if the object exists" do
-        described_class.s3.client.stub_responses(:get_object_acl, Aws::PageableResponse)
-        expect(described_class.s3).to exist("some-bucket", "some-key")
-      end
-
-      it "is false if AWS raises a NoSuchKey error" do
-        described_class.s3.client.stub_responses(:get_object_acl, "NoSuchKey")
-        expect(described_class.s3).to_not exist("some-bucket", "some-key")
-      end
-    end
-
-    describe "#create_if_missing" do
-      it "validates presence of bucket, key and body keys in param hash" do
+      logs = capture_logs_from(described_class.logger, level: :debug, formatter: :json) do
         expect do
-          described_class.s3.create_if_missing(bucket: "some-bucket")
-        end.to raise_error(ArgumentError)
-        expect do
-          described_class.s3.create_if_missing(bucket: "some-bucket", key: "some_key")
-        end.to raise_error(ArgumentError)
+          described_class.sts_client.assume_role(
+            role_arn: "arn:aws:iam::05408855555:role/Testing",
+            role_session_name: "testing",
+          )
+        end.to raise_error(Aws::STS::Errors::BadRequest)
       end
-
-      it "creates a new object when one does not exist" do
-        described_class.s3.client.stub_responses(:get_object_acl, "NoSuchKey") # used by s3_object_exists?
-        described_class.s3.client.stub_responses(:head_object, Aws::PageableResponse) # used by s3_object_metadata
-
-        result = described_class.s3.create_if_missing(bucket: "some-bucket", key: "some-key", body: "some-body")
-        expect(result).to be_a(Seahorse::Client::Response)
-        expect(result).to eq(Aws::PageableResponse)
-      end
-
-      it "does not overwrite an existing object" do
-        described_class.s3.client.stub_responses(:head_object, Aws::PageableResponse) # used by s3_object_metadata
-        expect(described_class.s3.client).to_not receive(:put_object)
-
-        result = described_class.s3.create_if_missing(bucket: "some-bucket", key: "some-key", body: "some-body")
-        expect(result).to be_a(Seahorse::Client::Response)
-        expect(result).to eq(Aws::PageableResponse)
-      end
-    end
-
-    describe "#put" do
-      it "validates presence of bucket, key and body keys in param hash" do
-        expect { described_class.s3.put(bucket: "some-bucket") }.to raise_error(ArgumentError)
-        expect { described_class.s3.put(bucket: "some-bucket", key: "some_key") }.to raise_error(ArgumentError)
-      end
-
-      it "puts an s3 object to the given bucket with the given key and body" do
-        result = described_class.s3.put(bucket: "some-bucket", key: "some-key", body: "some-body")
-        expect(result).to be_truthy
-      end
-    end
-
-    describe "#delete" do
-      it "deletes an s3 object in the given bucket with the given key and returns the response" do
-        described_class.s3.client.stub_responses(:delete_object, Aws::S3::Types::DeleteObjectOutput.new)
-        result = described_class.s3.delete("some-bucket", "some-key")
-        expect(result).to be_a(Seahorse::Client::Response)
-        expect(result.data).to be_a(Aws::S3::Types::DeleteObjectOutput)
-      end
-    end
-
-    describe "#presigned_get_url" do
-      it "returns an URL containing the bucket name and key name" do
-        url = described_class.s3.presigned_get_url("frabjous", "day")
-        expect(url).to start_with("https://frabjous.s3.us-east-2.amazonaws.com/day?X-Amz-Algorithm")
-      end
-    end
-
-    describe "#get_string" do
-      it "returns the body of an existing object" do
-        body = described_class.s3.get_string("some-bucket", "some-key")
-        expect(body).to be_a(String)
-      end
-    end
-
-    describe "#get_stream" do
-      it "returns the body of an existing object" do
-        body = described_class.s3.get_stream("some-bucket", "some-key")
-        expect(body).to be_a(StringIO)
-      end
-    end
-
-    describe "#bucket_and_key_from_uri" do
-      it "returns the bucket and key from an s3 URL" do
-        url = "s3://potato/mashed.png"
-        expect(described_class.s3.bucket_and_key_from_uri(url)).to eq(["potato", "mashed.png"])
-      end
-
-      it "returns the bucket and key from an HTTP URL" do
-        url = "https://potato.s3.amazonaws.com/fld/mashed.png"
-        expect(described_class.s3.bucket_and_key_from_uri(url)).to eq(["potato", "fld/mashed.png"])
-      end
-
-      it "returns the bucket and key from an HTTP URL with the region" do
-        url = "https://potato.s3.us-east-2.amazonaws.com/fld/mashed.png"
-        expect(described_class.s3.bucket_and_key_from_uri(url)).to eq(["potato", "fld/mashed.png"])
-      end
-
-      it "raises if the URL is not an s3 or HTTP URL" do
-        expect do
-          described_class.s3.bucket_and_key_from_uri("http://potato.com/mashed.jpg")
-        end.to raise_error(ArgumentError)
-      end
-    end
-
-    describe "#signed_url_from_uri" do
-      it "raises if the uri is not an s3 uri" do
-        expect do
-          described_class.s3.signed_url_from_uri("http://potato.com/mashed.jpg")
-        end.to raise_error(ArgumentError)
-      end
-
-      it "returns a signed url" do
-        expect(described_class.s3.signed_url_from_uri("s3://potato/mashed.jpeg")).to start_with(
-          "https://potato.s3.us-east-2.amazonaws.com/mashed.jpeg",
-        )
-      end
-
-      it "handles signing options" do
-        url = described_class.s3.signed_url_from_uri(
-          "s3://potato/mashed.jpeg",
-          response_content_disposition: "attachment",
-        )
-        expect(url).to include(
-          "response-content-disposition=attachment",
-        )
-      end
-    end
-
-    describe "upload_url" do
-      it "uploads non-s3 images to s3" do
-        url = Faker::Webhookdb.image_url(filename: CGI.escape("special <-> CHARS"), ext: "png")
-
-        get_req = stub_request(:get, url).
-          and_return(status: 200, body: "", headers: {"Content-Type" => "image/jpeg"})
-        expect(described_class.s3).to receive(:put).once
-
-        new_url = described_class.s3.upload_url("my-prefix", "buckit", url)
-
-        expected = %r{https://buckit\.s3\.amazonaws\.com/test/my-prefix/[a-z0-9]+-special-chars\.png}
-        expect(new_url).to match(expected)
-        expect(get_req).to have_been_made
-      end
-
-      it "uses a class name as the prefix if not a string" do
-        url = Faker::Webhookdb.image_url
-
-        stub_request(:get, url).
-          and_return(status: 200, body: "", headers: {"Content-Type" => "image/jpeg"})
-        expect(described_class.s3).to receive(:put).once
-
-        new_url = described_class.s3.upload_url(Webhookdb::Customer, "buckit", url)
-
-        expect(new_url).to include("/customer/")
-      end
-
-      it "leaves out an extension if the source has none" do
-        url = "http://co.co/image"
-
-        stub_request(:get, url).
-          and_return(status: 200, body: "", headers: {"Content-Type" => "image/jpeg"})
-        expect(described_class.s3).to receive(:put).once
-
-        new_url = described_class.s3.upload_url(Webhookdb::Customer, "buckit", url)
-
-        expect(new_url).to end_with("-image")
-      end
-
-      it "does not re-upload images on s3" do
-        already_on_s3 = Faker::Webhookdb.s3_url
-        new_url = described_class.s3.upload_url(Webhookdb::Customer, "buckit", already_on_s3)
-        expect(new_url).to eq(already_on_s3)
-      end
-
-      it "manipulates dropbox links to raw=1" do
-        req = stub_request(:get, "https://www.dropbox.com/s/some-folder/my-image.png?raw=1").
-          and_return(status: 200, body: "", headers: {"Content-Type" => "image/png"})
-        expect(described_class.s3).to receive(:put).once
-
-        described_class.s3.upload_url(Webhookdb::Customer, "buckit",
-                                      "https://www.dropbox.com/s/some-folder/my-image.png?dl=0",)
-
-        expect(req).to have_been_made
-      end
-
-      it "does not fail for malformed urls" do
-        expect do
-          described_class.s3.upload_url(Webhookdb::Customer, "buckit", "/some-folder/my-image.png?dl=0")
-        end.to raise_error(/be an absolute/)
-      end
+      expect(logs).to contain_exactly(
+        include_json(
+          level: "warn",
+          name: "Webhookdb::AWS",
+          message: "aws_assume_role",
+          context: {
+            aws_client: "STS",
+            http_response_code: 400,
+            elapsed: be_positive,
+            request_params: "role_arn:\"arn:aws:iam::05408855555:role/Testing\",role_session_name:\"testing\"",
+            error_class: "Aws::STS::Errors::BadRequest",
+            error_message: "Aws::STS::Errors::BadRequest",
+          },
+        ),
+      )
     end
   end
 end
