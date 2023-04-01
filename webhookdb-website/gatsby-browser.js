cf0b8d343ea93a25ed3b994efe0ba2b5d3ac9453
Rob Galanakis <rob.galanakis@gmail.com>
2023-04-01 09:38:19 -0700

    Remove webhookdb-website, it's now in its own repo

--- webhookdb-website/gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-import { ContactUsProvider } from "./src/components/useContactUs";
-import React from "react";
-
-export const wrapRootElement = ({ element }) => (
-  <ContactUsProvider>{element}</ContactUsProvider>
-);
--- webhookdb-website/src/components/staticData.js
+++
@@ -1,4 +0,0 @@
-export default {
-  announcementBlog: `https://lithic.tech/blog/2022-03/announcing-webhookdb`,
-  introVideo: "https://fast.wistia.com/embed/medias/lrox7uw103",
-};
