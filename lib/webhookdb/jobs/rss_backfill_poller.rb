c0f13e7fe3ccca42cdee5212131f2c9b823d654d
Rob Galanakis <rob.galanakis@gmail.com>
2023-06-10 11:12:02 -0700

    Add RssBackfillPoller job

---
+++ lib/webhookdb/jobs/rss_backfill_poller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require "webhookdb/async/scheduled_job"
+require "webhookdb/jobs"
+
+# RSS feeds don't generally update more than every hour.
+# Poll all the 'simple' feeds hourly by enqueing a backfill.
+class Webhookdb::Jobs::RssBackfillPoller
+  extend Webhookdb::Async::ScheduledJob
+
+  cron "11 * * * *" # At minute 11
+  splay 5.seconds
+
+  SERVICES = ["atom_single_feed_v1"].freeze
+
+  def _perform
+    Webhookdb::ServiceIntegration.dataset.where_each(service_name: SERVICES) do |sint|
+      sint.publish_immediate("backfill", sint.id, {incremental: true})
+    end
+  end
+end
