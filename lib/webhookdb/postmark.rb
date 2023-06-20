a3ff99c964e116f07611a322c2ad5df772d2377a
Rob Galanakis <rob.galanakis@gmail.com>
2023-06-20 12:36:30 -0700

    Add Postmark inbound and outbound integrations

--- lib/webhookdb/postmark.rb
+++ lib/webhookdb/postmark.rb
@@ -16,6 +16,10 @@ module Webhookdb::Postmark
   configurable(:postmark) do
     setting :api_key, "postmark-key"
     setting :sandbox_mode, true
+    # See https://postmarkapp.com/support/article/800-ips-for-firewalls#webhooks
+    setting :allowed_ips,
+            ["127.0.0.1", "3.134.147.250", "50.31.156.6", "50.31.156.77", "18.217.206.57"],
+            convert: ->(s) { s.split.map(&:strip) }
   end
 
   # Return a new Postmark client.
@@ -52,5 +56,11 @@ module Webhookdb::Postmark
   def self.format_to(email, name)
     return email if name.blank?
     return "#{name} <#{email}>"
+  end
+
+  def self.webhook_response(request)
+    ip = request.ip
+    allowed = self.allowed_ips.include?(ip)
+    return allowed ? Webhookdb::WebhookResponse.ok : Webhookdb::WebhookResponse.error("invalid ip")
   end
 end
