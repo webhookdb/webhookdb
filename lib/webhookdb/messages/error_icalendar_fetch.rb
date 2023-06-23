027e83364427c7ba2a3f12003b35fd6ee0a9e2a0
Rob Galanakis <rob.galanakis@gmail.com>
2023-06-23 14:28:10 -0700

    Send an email when icalendar sync fails

---
+++ lib/webhookdb/messages/error_icalendar_fetch.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "webhookdb/message/template"
+
+class Webhookdb::Messages::ErrorIcalendarFetch < Webhookdb::Message::Template
+  def self.fixtured(_recipient)
+    sint = Webhookdb::Fixtures.service_integration.create
+    return self.new(sint, "calendar123",
+                    response_status: 403, request_url: "/foo", request_method: "GET", response_body: "hi",)
+  end
+
+  def initialize(service_integration, external_calendar_id, request_url:, request_method:, response_status:,
+    response_body:)
+    @service_integration = service_integration
+    @external_calendar_id = external_calendar_id
+    @request_url = request_url
+    @request_method = request_method
+    @response_status = response_status
+    @response_body = response_body
+    super()
+  end
+
+  def signature
+    ehash = Digest::MD5.hexdigest(@kw.to_s)
+    return "msg-#{self.full_template_name}-sint:#{@service_integration.id}-eid:#{@external_calendar_id}-err:#{ehash}"
+  end
+
+  def template_folder = "errors"
+  def template_name = "icalendar_fetch"
+
+  def liquid_drops
+    return super.merge(
+      service_name: @service_integration.service_name,
+      opaque_id: @service_integration.opaque_id,
+      request_method: @request_method,
+      request_url: @request_url,
+      response_status: @response_status,
+      response_body: @response_body,
+      external_calendar_id: @external_calendar_id,
+    )
+  end
+end
