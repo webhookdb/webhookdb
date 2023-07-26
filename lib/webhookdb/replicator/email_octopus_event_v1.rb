ed6ec74016381cf5538e4d22e8167d5474184f17
natalithic <69214620+natalithic@users.noreply.github.com>
2023-07-26 09:25:06 -0700

    Rework email octopus webhook handling (#749)
    * Remove 'campaign_event' integration, it's now just 'event'
    * Only 'event' uses webhooks, and updates contacts as needed
    * Only 'list' is a root-level integration
    * Tests that `on_dependency_webhook_upsert` is implemented when needed

---
+++ lib/webhookdb/replicator/email_octopus_event_v1.rb
@@ -0,0 +1,241 @@
+# frozen_string_literal: true
+
+require "webhookdb/email_octopus"
+
+class Webhookdb::Replicator::EmailOctopusEventV1 < Webhookdb::Replicator::Base
+  include Appydays::Loggable
+
+  # @return [Webhookdb::Replicator::Descriptor]
+  def self.descriptor
+    return Webhookdb::Replicator::Descriptor.new(
+      name: "email_octopus_event_v1",
+      ctor: ->(sint) { Webhookdb::Replicator::EmailOctopusEventV1.new(sint) },
+      feature_roles: [],
+      resource_name_singular: "Email Octopus Event",
+      dependency_descriptor: Webhookdb::Replicator::EmailOctopusListV1.descriptor,
+      supports_webhooks: true,
+      supports_backfill: true,
+    )
+  end
+  BUILD_EVENT_MD5 = Webhookdb::Replicator::Column::IsomorphicProc.new(
+    ruby: lambda do |resource:, **|
+      # MD5 includes occurred_at, event_type, email_octopus_contact_id, and email_octopus_campaign_id.
+      md5 = Digest::MD5.new
+      md5.update(resource.fetch("occurred_at"))
+      md5.update(resource.fetch("event_type"))
+      md5.update(resource.dig("contact", "id"))
+      md5.update(resource.fetch("campaign_id", "missing"))
+      md5.hexdigest
+    end,
+  )
+
+  def _remote_key_column
+    return Webhookdb::Replicator::Column.new(:unique_id, UUID, optional: true, defaulter: BUILD_EVENT_MD5)
+  end
+
+  def _denormalized_columns
+    return [
+      Webhookdb::Replicator::Column.new(:email_octopus_contact_id, TEXT, data_key: ["contact", "id"]),
+      Webhookdb::Replicator::Column.new(:contact_email_address, TEXT, data_key: ["contact", "email_address"]),
+      Webhookdb::Replicator::Column.new(:email_octopus_campaign_id, TEXT, data_key: ["campaign_id"], optional: true),
+      Webhookdb::Replicator::Column.new(:event_type, TEXT),
+      Webhookdb::Replicator::Column.new(:occurred_at, TIMESTAMP, index: true, converter: :time),
+    ]
+  end
+
+  def _timestamp_column_name
+    return :occurred_at
+  end
+
+  def _upsert_webhook(request, upsert: true)
+    return super unless request.body.is_a?(Array)
+
+    # If the body is an array, it means we are upserting data from webhooks and have to transform the data
+    # in order to be able to upsert.
+    new_bodies = request.body.map do |wh|
+      new_body = {
+        "contact" => {
+          "id" => wh.fetch("contact_id"),
+          "email_address" => wh.fetch("contact_email_address"),
+        },
+        "occurred_at" => wh.fetch("occurred_at"),
+        "event_type" => wh.fetch("type"),
+      }
+
+      # "campaign_id" isn't always populated in the webhoooks, it is only there on event types that are tied
+      # to a specific "campaign", (that's Email Octopus's word for an email message), like "bounced" or "opened"
+      if (campaign_id = wh["campaign_id"])
+        new_body["campaign_id"] = campaign_id
+      end
+      new_body
+    end
+
+    new_bodies.each do |b|
+      new_request = request.dup
+      new_request.body = b
+      super(new_request, upsert:)
+    end
+
+    list_svc = self.service_integration.depends_on.replicator
+    contact_sint = list_svc.find_dependent("email_octopus_contact_v1")
+    return unless contact_sint
+    contact_svc = contact_sint.replicator
+    # For events that pertain to a contact being created or updated in some way, we also upsert them using
+    # the contact integration so that the new information can be recorded in the contact table
+    contact_event_types = ["contact.created", "contact.updated", "contact.deleted"]
+    contact_events = request.body.filter { |nb| contact_event_types.include?(nb.fetch("type")) }
+    return if contact_events.empty?
+    contact_request = request.dup
+    contact_request.body = contact_events
+    contact_svc._upsert_webhook(contact_request, upsert:)
+  end
+
+  def _resource_and_event(request)
+    return request.body, nil
+  end
+
+  def _update_where_expr
+    # 'occurred_at' is the timestamp, but it's used in the unique id,
+    # so there's no way to use it as part of the conditional upsert.
+    # So events are effectively immutable, and we know we shouldn't bother overwriting what's already in the DB.
+    return Sequel[false]
+  end
+
+  def _webhook_response(request)
+    signature_header = request.env["HTTP_EMAILOCTOPUS_SIGNATURE"]
+    return Webhookdb::WebhookResponse.error("missing signature") if signature_header.nil?
+
+    request.body.rewind
+    data = request.body.read
+    verified = Webhookdb::EmailOctopus.verify_webhook(data, signature_header, self.service_integration.webhook_secret)
+    return Webhookdb::WebhookResponse.ok if verified
+    return Webhookdb::WebhookResponse.error("invalid signature")
+  end
+
+  def calculate_webhook_state_machine
+    if (step = self.calculate_dependency_state_machine_step(dependency_help: ""))
+      return step
+    end
+    step = Webhookdb::Replicator::StateMachineStep.new
+    unless self.service_integration.webhook_secret.present?
+      step.output = %(You are about to start replicating #{self.resource_name_plural} into WebhookDB.
+We've made an endpoint available for #{self.resource_name_singular} webhooks:
+
+#{self._webhook_endpoint}
+
+From your Email Octopus dashboard, go to Account Settings -> Integrations & API.
+Then click the 'Manage' button next to 'Webhooks'.
+Then under the "Endpoints" header, click "Add endpoint"
+In the "URL" field you can enter the URL above.
+Then check boxes for all events, because we want this webhook to listen for everything. This includes
+"Created", "Updated", and "Deleted" under the "Contact events to send" header and "Clicked", "Opened", "Bounced",
+"Complained", and "Unsubscribed" under the "Email events to send" header.
+Save the endpoint.
+You'll be dropped back on the Webhooks page.
+Click 'View Secret' next to the endpoint you added, and Copy it.
+We'll use it for webhook verification.
+      )
+      return step.secret_prompt("webhook secret").webhook_secret(self.service_integration)
+    end
+
+    step.output = %(Great! WebhookDB is now listening for #{self.resource_name_singular} webhooks.
+#{self._query_help_output}
+In order to backfill existing #{self.resource_name_plural}, run this from a shell:
+
+  #{self._backfill_command}
+    )
+    return step.completed
+  end
+
+  def calculate_backfill_state_machine
+    if (step = self.calculate_dependency_state_machine_step(dependency_help: ""))
+      return step
+    end
+    step = Webhookdb::Replicator::StateMachineStep.new
+    # We're using the API Key from the dependency, we don't need to ask for it here
+    step.output = %(Great! We are going to start replicating your #{self.resource_name_plural}.
+#{self._query_help_output}
+    )
+    return step.completed
+  end
+
+  def on_dependency_webhook_upsert(_replicator, _payload, *)
+    return
+  end
+
+  CAMPAIGN_EVENT_TYPES = [
+    "bounced",
+    "clicked",
+    "complained",
+    "opened",
+    "unsubscribed",
+  ].freeze
+
+  def _backfillers
+    list_sint = self.service_integration.depends_on
+    unless list_sint.backfill_key.present?
+      raise Webhookdb::Replicator::CredentialsMissing,
+            "This integration requires that the Email Octopus List integration has a valid API Key"
+    end
+
+    campaign_sint = list_sint.replicator.find_dependent!("email_octopus_campaign_v1")
+    campaign_svc = campaign_sint.replicator
+    backfillers = campaign_svc.admin_dataset(timeout: :fast) do |campaign_ds|
+      campaign_ds.select(:email_octopus_id).flat_map do |campaign|
+        CAMPAIGN_EVENT_TYPES.map do |event_type|
+          EventBackfiller.new(
+            event_svc: self,
+            campaign_id: campaign[:email_octopus_id],
+            api_key: list_sint.backfill_key,
+            event_type:,
+          )
+        end
+      end
+    end
+    return backfillers
+  end
+
+  # Because "Event" is an abstraction we've created and does not actually exist in the Email Octopus API,
+  # it might seem strange that we are able to "backfill" the resources at all. However, the API has a number of
+  # endpoints that they call "campaign reports," which return lists of contacts that have engaged with a campaign
+  # in the specified way (e.g. bounced, opened, etc.) In this way, we can retrieve timestamped records of "events" that
+  # have already occurred. The only available campaign report endpoint that we don't hit is "sent", which notes the time
+  # at which each campaign was sent to each contact. This information doesn't come in through webhooks at all and the
+  # timestamps closely match the "sent_at" field in the campaign row, so we have opted not to track it through backfill.
+  class EventBackfiller < Webhookdb::Backfiller
+    include Webhookdb::Backfiller::Bulk
+
+    def initialize(event_svc:, campaign_id:, api_key:, event_type:)
+      @event_svc = event_svc
+      @campaign_id = campaign_id
+      @api_key = api_key
+      @event_type = event_type
+      super()
+    end
+
+    def upserting_replicator = @event_svc
+    def upsert_page_size = 500
+
+    def prepare_body(body)
+      body["campaign_id"] = @campaign_id
+      body["event_type"] = "contact.#{@event_type}"
+    end
+
+    def fetch_backfill_page(pagination_token, **_kwargs)
+      limit = Webhookdb::EmailOctopus.page_size
+      base_url = "https://emailoctopus.com"
+      # rubocop:disable Layout/LineLength
+      endpoint_path = pagination_token || "/api/1.6/campaigns/#{@campaign_id}/reports/#{@event_type}?api_key=#{@api_key}&limit=#{limit}"
+      # rubocop:enable Layout/LineLength
+      response = Webhookdb::Http.get(
+        base_url + endpoint_path,
+        logger: @event_svc.logger,
+        timeout: Webhookdb::EmailOctopus.http_timeout,
+      )
+      data = response.parsed_response
+      # if no data is returned from endpoint, the "paging" and "data" values are both empty arrays
+      next_page_link = data.fetch("paging").empty? ? nil : data.dig("paging", "next")
+      return data["data"], next_page_link
+    end
+  end
+end
--- spec/webhookdb/replicator/email_octopus_campaign_v1_spec.rb
+++ spec/webhookdb/replicator/email_octopus_campaign_v1_spec.rb
@@ -31,38 +31,9 @@ RSpec.describe Webhookdb::Replicator::EmailOctopusCampaignV1, :db do
     let(:supports_row_diff) { false }
   end
 
-  it_behaves_like "a replicator that verifies backfill secrets" do
-    let(:correct_creds_sint) do
-      Webhookdb::Fixtures.service_integration.create(
-        service_name: "email_octopus_campaign_v1",
-        backfill_key: "bfkey",
-      )
-    end
-    let(:incorrect_creds_sint) do
-      Webhookdb::Fixtures.service_integration.create(
-        service_name: "email_octopus_campaign_v1",
-        backfill_key: "bfkey_wrong",
-      )
-    end
-
-    let(:success_body) do
-      <<~R
-        {
-          "data": [],
-          "paging": {}
-        }
-      R
-    end
-
-    def stub_service_request
-      return stub_request(:get, "https://emailoctopus.com/api/1.6/campaigns?api_key=bfkey&limit=100").
-          to_return(status: 200, body: success_body, headers: {})
-    end
-
-    def stub_service_request_error
-      return stub_request(:get, "https://emailoctopus.com/api/1.6/campaigns?api_key=bfkey_wrong&limit=100").
-          to_return(status: 401, body: "", headers: {})
-    end
+  it_behaves_like "a replicator dependent on another", "email_octopus_campaign_v1",
+                  "email_octopus_list_v1" do
+    let(:no_dependencies_message) { "This integration requires Email Octopus Lists to sync" }
   end
 
   it_behaves_like "a replicator that can backfill", "email_octopus_campaign_v1" do
@@ -166,50 +137,11 @@ RSpec.describe Webhookdb::Replicator::EmailOctopusCampaignV1, :db do
     end
   end
 
-  it_behaves_like "a replicator with dependents", "email_octopus_campaign_v1", "email_octopus_campaign_event_v1" do
-    let(:body) do
-      JSON.parse(<<~J)
-        {
-          "id": "0",
-          "status": "SENT",
-          "name": "Foo",
-          "subject": "Bar",
-          "to": [
-            "00000000-0000-0000-0000-000000000001",
-            "00000000-0000-0000-0000-000000000002"
-          ],
-          "from": {
-            "name": "John Doe",
-            "email_address": "john.doe@gmail.com"
-          },
-          "content": {
-            "html": "<html>Foo Bar<html>",
-            "plain_text": "Foo Bar"
-          },
-          "created_at": "2023-07-02T15:40:12+00:00",
-          "sent_at": "2023-07-03T15:40:12+00:00"
-        }
-      J
-    end
-    let(:can_track_row_changes) { true }
-    let(:expected_insert) do
-      {
-        name: "Foo",
-        created_at: match_time("2023-07-02T15:40:12+00:00"),
-        sent_at: match_time("2023-07-03T15:40:12+00:00"),
-        row_updated_at: match_time(:now),
-        data: body.to_json,
-        email_octopus_id: "0",
-        from_email_address: "john.doe@gmail.com",
-        from_name: "John Doe",
-        status: "SENT",
-        subject: "Bar",
-      }
-    end
-  end
-
   describe "state machine calculation" do
-    let(:sint) { Webhookdb::Fixtures.service_integration.create(service_name: "email_octopus_campaign_v1") }
+    let(:list_sint) { Webhookdb::Fixtures.service_integration.create(service_name: "email_octopus_list_v1") }
+    let(:sint) do
+      Webhookdb::Fixtures.service_integration.depending_on(list_sint).create(service_name: "email_octopus_campaign_v1")
+    end
     let(:svc) { Webhookdb::Replicator.create(sint) }
 
     describe "calculate_backfill_state_machine" do
@@ -226,23 +158,10 @@ RSpec.describe Webhookdb::Replicator::EmailOctopusCampaignV1, :db do
         return stub_request(:get, "https://emailoctopus.com/api/1.6/campaigns?api_key=bfkey&limit=100").
             to_return(status: 200, body: success_body, headers: {"Content-Type" => "application/json"})
       end
-      it "asks for backfill key" do
-        sm = sint.replicator.calculate_backfill_state_machine
-        expect(sm).to have_attributes(
-          needs_input: true,
-          prompt: "Paste or type your API Key here:",
-          prompt_is_secret: true,
-          post_to_url: end_with("/service_integrations/#{sint.opaque_id}/transition/backfill_key"),
-          complete: false,
-          output: match("In order to replicate Email Octopus Campaigns into WebhookDB, we need an API Key."),
-        )
-      end
 
-      it "confirms reciept of backfill key, returns org database info" do
+      it "returns org database info" do
         sint.backfill_key = "bfkey"
-        res = stub_service_request
         sm = sint.replicator.calculate_backfill_state_machine
-        expect(res).to have_been_made
         expect(sm).to have_attributes(
           needs_input: false,
           prompt: "",
