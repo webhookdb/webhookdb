d00ee6048edc1b289feec2514e294e141912f401
Rob Galanakis <rob.galanakis@gmail.com>
2023-08-13 21:05:34 -0700

    Improve logging for icalendar sync
    Followup for https://github.com/lithictech/webhookdb-api/issues/762
    
    Also fixes this line:
    
    ```rb
    self.logger.info "enqueing_icalendar_sync", external_id:
    Webhookdb::Jobs::IcalendarSync.perform_async(sint.id, external_id)
    ```
    
    This was interpreted as:
    
    ```rb
    self.logger.info "enqueing_icalendar_sync", external_id: Webhookdb::Jobs::IcalendarSync.perform_async(sint.id, external_id)
    ```
    
    So the logs had an external ID tied to the job ID, not the
    external id value.

--- lib/webhookdb/jobs/icalendar_enqueue_syncs.rb
+++ lib/webhookdb/jobs/icalendar_enqueue_syncs.rb
@@ -13,10 +13,10 @@ class Webhookdb::Jobs::IcalendarEnqueueSyncs
     Webhookdb::ServiceIntegration.dataset.where_each(service_name: "icalendar_calendar_v1") do |sint|
       sint.replicator.admin_dataset do |ds|
         sint.replicator.rows_needing_sync(ds).each do |row|
-          external_id = row.fetch(:external_id)
+          calendar_external_id = row.fetch(:external_id)
           self.with_log_tags(sint.log_tags) do
-            self.logger.info "enqueing_icalendar_sync", external_id:
-            Webhookdb::Jobs::IcalendarSync.perform_async(sint.id, external_id)
+            enqueued_job_id = Webhookdb::Jobs::IcalendarSync.perform_async(sint.id, calendar_external_id)
+            self.logger.info("enqueued_icalendar_sync", calendar_external_id:, enqueued_job_id:)
           end
         end
       end
