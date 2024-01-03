fea136f0bfedffbb309f9cb823cde4401e42812a
Rob Galanakis <rob.galanakis@gmail.com>
2024-01-02 16:17:51 -0800

    Get Docker mostly working

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 if [ "$1" = "worker" ]; then
-  bundle exec sidekiq -r ./config/sidekiq.rb -C ./config/sidekiq.yml
+  exec bundle exec sidekiq -r ./config/sidekiq.rb -C ./config/sidekiq.yml
 elif [ "$1" = "web" ]; then
-  bundle exec puma -C config/puma.rb
+  exec bundle exec puma -C config/puma.rb
 elif [ "$1" = "release" ]; then
-  bundle exec rake release
+  exec bundle exec rake release
 else
   exec "$@"
 fi
