92b957d481aff705030b5ad2dc9ab9e6125fb12c
rust-jvm-go <rustxjvmxgo@gmail.com>
2024-01-26 21:02:32 +0800

    New tests fail

--- spec/webhookdb/replicator/brevo_email_activity_unaggregated_events_v1_spec.rb
+++ spec/webhookdb/replicator/brevo_email_activity_unaggregated_events_v1_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Webhookdb::Replicator::BrevoEmailActivityUnaggregatedEventsV1, :d
       JSON.parse(<<~J)
         {
           "email": "example@example.com",
-          "date": "2024-01-23T09:36:46.364+08:00",
+          "date": "2024-01-23T09:34:13.916+08:00",
           "subject": "Brevo Test Transactional Mail",
           "messageId": "<202401230136.35720869946@smtp-relay.mailin.fr>",
           "event": "requests",
@@ -19,6 +19,40 @@ RSpec.describe Webhookdb::Replicator::BrevoEmailActivityUnaggregatedEventsV1, :d
       J
     end
     let(:expected_data) { body }
+  end
+
+  it_behaves_like "a replicator that prevents overwriting new data with old",
+                  "brevo_email_activity_unaggregated_events_v1" do
+    let(:old_body) do
+      JSON.parse(<<~J)
+        {
+          "email": "example@example.com",
+          "date": "2024-01-23T09:34:13.916+08:00",
+          "subject": "Brevo Test Transactional Mail",
+          "messageId": "<202401230136.35720869946@smtp-relay.mailin.fr>",
+          "event": "requests",
+          "tag": "",
+          "ip": "77.32.148.20",
+          "from": "example@example.com"
+        }
+      J
+    end
+    let(:new_body) do
+      JSON.parse(<<~J)
+        {
+          "email": "example@example.com",
+          "date": "2024-01-23T09:34:28.201+08:00",
+          "subject": "Brevo Test Transactional Mail",
+          "messageId": "<202401230136.35720869946@smtp-relay.mailin.fr>",
+          "event": "opened",
+          "tag": "",
+          "ip": "77.32.148.20",
+          "from": "example@example.com"
+        }
+      J
+    end
+    let(:expected_old_data) { old_body }
+    let(:expected_new_data) { new_body }
   end
 
   it_behaves_like "a replicator that verifies backfill secrets" do
@@ -51,17 +85,89 @@ RSpec.describe Webhookdb::Replicator::BrevoEmailActivityUnaggregatedEventsV1, :d
         to_return(status: 200, body: success_body, headers: {})
     end
 
+    def stub_service_request_error
+      return stub_request(:get, "https://api.brevo.com/v3/smtp/statistics/events?days=90").
+        with(headers: {"api-key" => "bfkey_wrong"}).
+        to_return(status: 401, body: "", headers: {})
+    end
+  end
+
+  it_behaves_like "a replicator that can backfill", "brevo_email_activity_unaggregated_events_v1" do
+    let(:api_url) { "https://api.brevo.com/v3" }
+    let(:page1_response) do
+      <<~R
+        {
+          "events": [
+              {
+                  "email": "example@example.com",
+                  "date": "2024-01-23T09:34:13.916+08:00",
+                  "subject": "Brevo Test Transactional Mail",
+                  "messageId": "<202401230134.33146748018@smtp-relay.mailin.fr>",
+                  "event": "requests",
+                  "tag": "",
+                  "ip": "77.32.148.20",
+                  "from": "example@example.com"
+              },
+              {
+                  "email": "example@example.com",
+                  "date": "2024-01-23T09:34:14.000+08:00",
+                  "subject": "Brevo Test Transactional Mail",
+                  "messageId": "<202401230134.33146748018@smtp-relay.mailin.fr>",
+                  "event": "delivered",
+                  "tag": "",
+                  "ip": "77.32.148.20",
+                  "from": "example@example.com"
+              },
+              {
+                  "email": "example@example.com",
+                  "date": "2024-01-23T09:34:28.201+08:00",
+                  "subject": "Brevo Test Transactional Mail",
+                  "messageId": "<202401230134.33146748018@smtp-relay.mailin.fr>",
+                  "event": "opened",
+                  "tag": "",
+                  "ip": "74.125.209.36",
+                  "from": "example@example.com"
+              },
+              {
+                  "email": "example@example.com",
+                  "date": "2024-01-23T19:10:10.063+08:00",
+                  "subject": "Brevo Test Transactional Mail",
+                  "messageId": "<202401230134.33146748018@smtp-relay.mailin.fr>",
+                  "event": "opened",
+                  "tag": "",
+                  "ip": "74.125.209.35",
+                  "from": "example@example.com"
+              }
+          ]
+        }
+      R
+    end
+    let(:page2_response) do
+      <<~R
+        {}
+      R
+    end
+    let(:expected_items_count) { 4 }
+    def stub_service_requests
+      return [
+        stub_request(:get, "https://api.brevo.com/v3").
+          with(headers: {"Authorization" => "Bearer bfkey"}).
+          to_return(status: 200, body: page1_response, headers: {"Content-Type" => "application/json"}),
+        stub_request(:get, "https://api.brevo.com/v3/smtp/statistics/events?limit=10&offset=0&sort=desc&event=requests&startDate=2024-01-22&endDate=2024-01-22").
+          to_return(status: 200, body: page2_response, headers: {"Content-Type" => "application/json"}),
+      ]
+    end
+
     def stub_empty_requests
       return [
         stub_request(:get, "https://api.brevo.com/v3/smtp/statistics/events?days=90").
-          to_return(status: 200, body: page3_response, headers: {"Content-Type" => "application/json"}),
+          to_return(status: 200, body: page2_response, headers: {"Content-Type" => "application/json"}),
       ]
     end
 
     def stub_service_request_error
       return stub_request(:get, "https://api.brevo.com/v3/smtp/statistics/events?days=90").
-        with(headers: {"api-key" => "bfkey_wrong"}).
-        to_return(status: 401, body: "", headers: {})
+        to_return(status: 400, body: "geh")
     end
   end
 
