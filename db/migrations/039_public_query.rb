a150d2ca301c7d49feb4cee0209fc84caf307027
Rob Galanakis <rob.galanakis@gmail.com>
2024-02-04 19:41:34 -0800

    Better on delete for custom query fk

--- db/migrations/039_public_query.rb
+++ db/migrations/039_public_query.rb
@@ -6,8 +6,8 @@ Sequel.migration do
       primary_key :id
       timestamptz :created_at, null: false, default: Sequel.function(:now)
       timestamptz :updated_at
-      foreign_key :organization_id, :organizations, null: false, unique: true
-      foreign_key :created_by_id, :customers
+      foreign_key :organization_id, :organizations, null: false, unique: true, on_delete: :cascade
+      foreign_key :created_by_id, :customers, on_delete: :set_null
       text :opaque_id, null: false, unique: true
       text :description, null: false
       text :sql, null: false
