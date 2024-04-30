0adb099afcac779dc2bdfc5d746281c9372d29ee
Rob Galanakis <rob.galanakis@gmail.com>
2024-04-30 10:04:23 -0700

    fix

--- lib/webhookdb/replicator/webhook_request.rb
+++ lib/webhookdb/replicator/webhook_request.rb
@@ -6,4 +6,12 @@ class Webhookdb::Replicator::WebhookRequest < Webhookdb::TypedStruct
   # When a webhook is processed synchronously, this will be set to the Rack::Request.
   # Normal (async) webhook processing does not have this available.
   attr_accessor :rack_request
+
+  JSON_KEYS = ["body", "headers", "path", "method"].freeze
+  def as_json
+    return JSON_KEYS.each_with_object({}) do |k, h|
+      v = self.send(k)
+      h[k] = v.as_json unless v.nil?
+    end
+  end
 end
